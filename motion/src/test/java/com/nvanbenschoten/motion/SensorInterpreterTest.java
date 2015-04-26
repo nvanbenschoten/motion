3d5792d7d044fce7f2bc255dd6c85855504ee04a
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2015-04-25 18:20:47 -0700

    Update testing and unmock android.hardware Sensor classes for tests. Bump minimum SDK version of library from 8 to 9

--- motion/src/test/java/com/nvanbenschoten/motion/SensorInterpreterTest.java
+++ motion/src/test/java/com/nvanbenschoten/motion/SensorInterpreterTest.java
@@ -9,12 +9,27 @@ import android.view.WindowManager;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.Constructor;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+/*
+ * Copyright 2014 Nathan VanBenschoten
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class SensorInterpreterTest {
 
     private final float ACCEPTABLE_FLOAT_DELTA = 0.0001f;
@@ -23,103 +38,83 @@ public class SensorInterpreterTest {
     public void testInterpretSensorEventPortraitWithNoOffset() throws Exception {
         SensorInterpreter sensorInterpreter = new SensorInterpreter();
         sensorInterpreter.setTiltSensitivity(2);
-        sensorInterpreter.setForwardTiltOffset(0);
+        sensorInterpreter.setTargetVector(new float[]{0.5f, 0.6f, 0.7f});
 
         Context context = mockRotationContext(Surface.ROTATION_0);
-        SensorEvent event = mockSensorEvent(3);
-        event.values[0] = 10f;
-        event.values[1] = 25f;
-        event.values[2] = 40f;
+        SensorEvent event = mockSensorEvent(new float[]{0.7f, 0.7f, 0.7f});
 
         float[] interpreted = sensorInterpreter.interpretSensorEvent(context, event);
 
-        assertArrayEquals(new float[]{10f, -0.55555f, -0.88888f}, interpreted, ACCEPTABLE_FLOAT_DELTA);
+        assertArrayEquals(new float[]{-0.19003f, 0.21814f, 0.02382f}, interpreted, ACCEPTABLE_FLOAT_DELTA);
     }
 
     @Test
     public void testInterpretSensorEventLandscapeRightWithNoOffset() throws Exception {
         SensorInterpreter sensorInterpreter = new SensorInterpreter();
         sensorInterpreter.setTiltSensitivity(2);
-        sensorInterpreter.setForwardTiltOffset(0);
+        sensorInterpreter.setTargetVector(new float[]{0.5f, 0.6f, 0.7f});
 
         Context context = mockRotationContext(Surface.ROTATION_90);
-        SensorEvent event = mockSensorEvent(3);
-        event.values[0] = 10f;
-        event.values[1] = 25f;
-        event.values[2] = 40f;
+        SensorEvent event = mockSensorEvent(new float[]{0.7f, 0.7f, 0.7f});
 
         float[] interpreted = sensorInterpreter.interpretSensorEvent(context, event);
 
-        assertArrayEquals(new float[]{10f, 0.88888f,  -0.55555f}, interpreted, ACCEPTABLE_FLOAT_DELTA);
+        assertArrayEquals(new float[]{1.0f, 0.04103f, -0.12279f}, interpreted, ACCEPTABLE_FLOAT_DELTA);
     }
 
     @Test
     public void testInterpretSensorEventLandscapeLeftWithNoOffset() throws Exception {
         SensorInterpreter sensorInterpreter = new SensorInterpreter();
         sensorInterpreter.setTiltSensitivity(2);
-        sensorInterpreter.setForwardTiltOffset(0);
+        sensorInterpreter.setTargetVector(new float[]{0.5f, 0.6f, 0.7f});
 
         Context context = mockRotationContext(Surface.ROTATION_270);
-        SensorEvent event = mockSensorEvent(3);
-        event.values[0] = 10f;
-        event.values[1] = 25f;
-        event.values[2] = 40f;
+        SensorEvent event = mockSensorEvent(new float[]{0.7f, 0.7f, 0.7f});
 
         float[] interpreted = sensorInterpreter.interpretSensorEvent(context, event);
 
-        assertArrayEquals(new float[]{10f, -0.88888f, 0.55555f}, interpreted, ACCEPTABLE_FLOAT_DELTA);
+        assertArrayEquals(new float[]{-0.92730f, -0.04103f, 0.12279f}, interpreted, ACCEPTABLE_FLOAT_DELTA);
     }
 
     @Test
     public void testInterpretSensorEventUpsideDownWithNoOffset() throws Exception {
         SensorInterpreter sensorInterpreter = new SensorInterpreter();
         sensorInterpreter.setTiltSensitivity(2);
-        sensorInterpreter.setForwardTiltOffset(0);
+        sensorInterpreter.setTargetVector(new float[]{0.5f, 0.6f, 0.7f});
 
         Context context = mockRotationContext(Surface.ROTATION_180);
-        SensorEvent event = mockSensorEvent(3);
-        event.values[0] = 10f;
-        event.values[1] = 25f;
-        event.values[2] = 40f;
+        SensorEvent event = mockSensorEvent(new float[]{0.7f, 0.7f, 0.7f});
 
         float[] interpreted = sensorInterpreter.interpretSensorEvent(context, event);
 
-        assertArrayEquals(new float[]{10f, 0.55555f, 0.88888f}, interpreted, ACCEPTABLE_FLOAT_DELTA);
-    }
-
-    @Test
-    public void testInterpretSensorEventPortraitWithOffset() throws Exception {
-        SensorInterpreter sensorInterpreter = new SensorInterpreter();
-        sensorInterpreter.setTiltSensitivity(2);
-        sensorInterpreter.setForwardTiltOffset(0.1f);
-
-        Context context = mockRotationContext(Surface.ROTATION_0);
-        SensorEvent event = mockSensorEvent(3);
-        event.values[0] = 10f;
-        event.values[1] = 25f;
-        event.values[2] = 40f;
-
-        float[] interpreted = sensorInterpreter.interpretSensorEvent(context, event);
-
-        assertArrayEquals(new float[]{10f, -0.75555f, -0.88888f}, interpreted, ACCEPTABLE_FLOAT_DELTA);
+        assertArrayEquals(new float[]{1.0f, -0.21815f, -0.02382f}, interpreted, ACCEPTABLE_FLOAT_DELTA);
     }
 
     @Test
     public void testInterpretSensorClampValues() throws Exception {
         SensorInterpreter sensorInterpreter = new SensorInterpreter();
         sensorInterpreter.setTiltSensitivity(1.5f);
-        sensorInterpreter.setForwardTiltOffset(0);
+        sensorInterpreter.setTargetVector(new float[]{0f, 0f, 0f});
 
-        Context context = mockRotationContext(Surface.ROTATION_180);
-        SensorEvent event = mockSensorEvent(3);
-        event.values[0] = 10f;
-        event.values[1] = 65f;
-        event.values[2] = -80f;
+        Context context = mockRotationContext(Surface.ROTATION_0);
+        SensorEvent event = mockSensorEvent(new float[]{3.1f, 0f, 3.1f});
 
         float[] interpreted = sensorInterpreter.interpretSensorEvent(context, event);
 
-        assertEquals("positive numbers over 1 clamp to 1", 1f, interpreted[1], ACCEPTABLE_FLOAT_DELTA);
-        assertEquals("negative numbers under -1 clamp to -1", -1f, interpreted[2], ACCEPTABLE_FLOAT_DELTA);
+        assertEquals("positive numbers over 1 clamp to 1", 1, interpreted[0], ACCEPTABLE_FLOAT_DELTA);
+        assertEquals("negative numbers under -1 clamp to -1", -1, interpreted[2], ACCEPTABLE_FLOAT_DELTA);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetInvalidTiltSensitivity() throws Exception {
+        SensorInterpreter sensorInterpreter = new SensorInterpreter();
+        sensorInterpreter.setTiltSensitivity(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetNegativeTiltSensitivity() throws Exception {
+        SensorInterpreter sensorInterpreter = new SensorInterpreter();
+        sensorInterpreter.setTiltSensitivity(-1);
     }
 
     private Context mockRotationContext(int rotation) {
@@ -134,12 +129,12 @@ public class SensorInterpreterTest {
         return context;
     }
 
-    private SensorEvent mockSensorEvent(int size) throws Exception {
-        SensorEvent event = Mockito.mock(SensorEvent.class);
+    private SensorEvent mockSensorEvent(float[] values) throws Exception {
+        Constructor<SensorEvent> c = SensorEvent.class.getDeclaredConstructor(int.class);
+        c.setAccessible(true);
 
-        Field valuesField = SensorEvent.class.getField("values");
-        valuesField.setAccessible(true);
-        valuesField.set(event, new float[size]);
+        SensorEvent event = c.newInstance(values.length);
+        System.arraycopy(values, 0, event.values, 0, values.length);
 
         return event;
     }
