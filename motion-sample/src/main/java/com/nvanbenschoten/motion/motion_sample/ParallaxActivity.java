54762d25615f58065ec13e5b06d05f6e22bd56cc
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2015-04-25 18:15:21 -0700

    Clean up sample application code

--- motion-sample/src/main/java/com/nvanbenschoten/motion/motion_sample/ParallaxActivity.java
+++ motion-sample/src/main/java/com/nvanbenschoten/motion/motion_sample/ParallaxActivity.java
@@ -18,7 +18,21 @@ import android.widget.Switch;
 
 import com.nvanbenschoten.motion.ParallaxImageView;
 
-
+/*
+ * Copyright 2014 Nathan VanBenschoten
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class ParallaxActivity extends Activity {
 
     @Override
@@ -73,9 +87,6 @@ public class ParallaxActivity extends Activity {
         public void onViewCreated(View view, Bundle savedInstanceState) {
             super.onViewCreated(view, savedInstanceState);
 
-            // Adjust the Parallax forward tilt adjustment
-            mBackground.setForwardTiltOffset(.35f);
-
             // Set SeekBar to change parallax intensity
             mSeekBar.setMax(10);
             mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -90,15 +101,16 @@ public class ParallaxActivity extends Activity {
                 @Override
                 public void onStopTrackingTouch(SeekBar seekBar) { }
             });
-            mSeekBar.setProgress(1);
+            mSeekBar.setProgress(2);
         }
 
         @Override
         public void onResume() {
             super.onResume();
 
-            if (mParallaxSet)
+            if (mParallaxSet) {
                 mBackground.registerSensorManager();
+            }
         }
 
         @Override
